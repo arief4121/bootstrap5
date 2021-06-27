26ca6960c77495826083bb202e3ee4da24b4f009
Arief4121 <muharief1221@gmail.com>
2021-06-27 21:11:42 +0800

    versi satu

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+        $('#profile').addClass("d-sm-inline")   
+    })
+    $("#button2").click(function(){
+        $('#profile').removeClass("d-sm-inline")   
+    })
+})
